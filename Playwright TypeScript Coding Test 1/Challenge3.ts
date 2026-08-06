9f4e29c448f3c52b9ab0503d16e30f8d88bea63f
Rupender Singh <rupender.singh@gmail.com>
2026-08-06 09:02:43 +0530

    Challenge 3.1

--- Playwright TypeScript Coding Test 1/Challenge3.ts
+++ Playwright TypeScript Coding Test 1/Challenge3.ts
@@ -1,10 +1,13 @@
 function buildPwBasicsRoleLocator(role: string, name: string): string {
     // write your code here
     const cleanRole = role.trim();
-    const cleanName = name.trim();
+    let cleanName = name.trim();
+    if (cleanName.length == 0) {
+        cleanName = "Submit";
+    }
     const locator: string = `page.getByRole('${cleanRole}', { name: '${cleanName}' })`;
 
     return locator;
 }
 
-console.log(buildPwBasicsRoleLocator(" textbox ", " Email Address "));
+console.log(buildPwBasicsRoleLocator(" textbox ", ""));
